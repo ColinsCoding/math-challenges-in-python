4a6db96e29869454c041ca21b74d5659a72bb0ae
Colin <Casey>
2024-10-24 19:29:47 -0700

    using Sympy

--- college_algebra.py
+++ college_algebra.py
@@ -3,25 +3,25 @@ from sympy import symbols, Eq, solve
 def solve_equation(equation, variable):
     return solve(equation, variable)
 
-def problem_1():
+def main():
+    # Define symbols
     h = symbols('h')
-    equation = Eq(1.5 * (2 - 4 * h), 6 * h)
-    solution = solve_equation(equation, h)
-    print(f"The solution for h is: {solution}")
-
-def problem_2():
     n = symbols('n')
-    equation2 = Eq(((4*n) + 2), 6 * (( 1/3 * n)) - 2/3)
-    solution2 = solve_equation(equation2, n)
-    print(f"The solution for n is: {solution2}")
+    k = symbols('k')
+    f = symbols('f')
 
-def probelm_3():
-    x = symbols('x')
-    equation3 = Eq(2 * (x + 3), 3 * (x - 2))
-    solution3 = solve_equation(equation3, x)
-    print(f"The solution for x is: {solution3}")
+    # List of equations and their variables
+    problems = [
+        (Eq(1.5 * (2 - 4 * h), 6 * h), h),
+        (Eq(((4*n) + 2), 6 * ((( 1/3 * n)) - 2/3)), n),
+        (Eq((1/4) * k, 3 * (((-1 / 4) * k) + 3)), k),
+        (Eq((4 * (.5 * f - 0.25)), 6 + f), f)
+    ]
+
+    # Solve each equation
+    for equation, variable in problems:
+        solution = solve_equation(equation, variable)
+        print(f"The solution for {variable} is: {solution}")
 
 if __name__ == "__main__":
-    problem_1()
-    problem_2()
-    problem_3()
+    main()
